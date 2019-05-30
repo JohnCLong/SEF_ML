034623fdfa4717c6653f4838d9af2f6c57fa15a7
JohnCLong <johncampbelllong@gmail.com>
2019-05-30 16:37:29 +0100

    added several pre-processing steps including: - creating a copy of data - removing all duplicate columns - selects the top 5 most correlated attributes
    added data exploration:
    - histogram
    - scatter matrix
    - correlation matrix

--- initial models/Data_Exploration.py
+++ initial models/Data_Exploration.py
@@ -64,18 +64,48 @@ forecast_demand = forecast_demand.loc[:, 'TSDF']
 forecast_generation = generation_day_ahead.loc[:, 'quantity']
 
 # combine all features into one data frame
-df = pd.concat([NIV, generation_per_type, apx_price, renewable_generation_forecast, forecast_demand,
+data = pd.concat([NIV, generation_per_type, apx_price, renewable_generation_forecast, forecast_demand,
                    generation_day_ahead, initial_demand_outturn, interconnectors, loss_of_load_probability,
                    market_index_data, wind_forecast, ], axis=1, sort=True)
 alldf = pd.concat([NIV, forecast_renewables, forecast_demand, forecast_generation, wind_forecast], axis=1, sort=True)
+df = data.copy()
 df = df.drop("intnemGeneration", axis=1)
+df.drop('settlementDate', axis=1)
 df.dropna(inplace=True)
+
+df = df.loc[:,~df.columns.duplicated()]
+
+def rename_duplicate_columns(data, duplicate):
+    cols = []
+    count = 1
+    for column in data.columns:
+        if column == duplicate:
+            cols.append(duplicate+np.str(count))
+            count += 1
+            continue
+    cols.append(column)
+    data.columns = cols
+    return
+
 
 # Get names of indexes for which column generation has value less thatn 10GW and drop
 indexNames = df[df['quantity'] < 10000].index
 df.drop(indexNames, inplace=True)
 df = df.rename({'indicativeNetImbalanceVolume': 'NIV', 'quantity': 'Generation'}, axis='columns')
 
-cols_all = ['Biomass', 'HydroPumpedStorage', 'Other', 'Solar', 'solar', 'wind_off', 'APXPrice', 'initialWindForecast']
+# calculate the correlation matrix, isolate the NIV correlations and then order by the abs value (descending)
+correlation_matrix = df.corr()
+cm_NIV = correlation_matrix['NIV']
+cm_NIV = cm_NIV.reindex(cm_NIV.abs().sort_values(ascending=False).index)
+# create a new list of the two 5 most correlated values (starting at 1 as list_of_attributes[0] = 'NIV'
+list_of_rows = cm_NIV.index.values
+attributes = list_of_rows[1:6]
+
+df[attributes].hist(bins=50, figsize=(20, 15))
+plt.show()
+
+scatter_matrix(df[attributes], figsize=(20, 18))
+plt.show()
+
 
 scatter_matrix(df)
