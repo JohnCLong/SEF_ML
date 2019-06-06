b7eae5dac5958da10de2038bd73822ddf7338158
JohnCLong <johncampbelllong@gmail.com>
2019-06-06 19:19:02 +0100

    funciton for data preprpocessing.

--- initial models/All_models.py
+++ initial models/All_models.py
@@ -51,111 +51,148 @@ wind_generation_forecast_and_outturn.sort_index(inplace=True)
 # ---------------------------------------------------------------------
 # Data Pre-processing
 # function to sum columns
+raw_data = pd.concat([generation_per_type, apx_price, renewable_generation_forecast, forecast_demand,
+                      generation_day_ahead, derived_system_wide_data, forecast_day_and_day_ahead_demand_data,
+                      initial_demand_outturn, interconnectors, loss_of_load_probability,
+                      market_index_data, wind_generation_forecast_and_outturn], axis=1, sort=True)
 
 
-def sum_columns(df, columns):
-    return df.loc[:, columns].sum(axis=1)
+def preprocess_features(raw_data):
+    """Prepares input features from California housing data set.
+
+    Args:
+    whole_dataframe: A Pandas DataFrame expected to contain data
+      from the the "df".
+    Returns:
+    A DataFrame that contains the features to be used for the model, including
+    synthetic features.
+    """
+
+    # Create a copy of the raw data and then drop all duplicates and label data.
+    processed_features = raw_data.copy()
+    processed_features = processed_features.loc[:, ~processed_features.columns.duplicated()]
+
+    # Drop unwanted data or fill if applicable
+    indexNames = processed_features[processed_features['quantity'] < 10000].index
+    processed_features.drop(indexNames, inplace=True)
+    processed_features.drop("intnemGeneration", axis=1, inplace=True)
+    processed_features.drop('settlementDate', axis=1, inplace=True)
+    processed_features.drop('systemSellPrice', axis=1, inplace=True)
+    processed_features.drop('sellPriceAdjustment', axis=1, inplace=True)
+    processed_features.drop('FossilOil', axis=1, inplace=True)
+    processed_features['initialWindForecast'].fillna(method='ffill', inplace=True)
+    processed_features['latestWindForecast'].fillna(method='ffill', inplace=True)
+    processed_features['reserveScarcityPrice'].fillna(0, inplace=True)
+    processed_features['drm2HourForecast'].fillna(processed_features['drm2HourForecast'].mean(), inplace=True)
+    processed_features['lolp1HourForecast'].fillna(0, inplace=True)
+    processed_features.dropna(inplace=True)
 
 
-def subtract_columns(df, a, b):
-    return df[a] - df[b]
+
+    # Separate targets and features.
+    processed_target = processed_features['indicativeNetImbalanceVolume'].copy()
+
+    # Create a synthetic features.
+    processed_features.loc[:, 'RenewablePrediction'] = (processed_features.loc[:, 'solar'] +
+                                                        processed_features.loc[:, 'wind_off'] +
+                                                        renewable_generation_forecast.loc[:, 'wind_on'])
+    processed_features['Val_Diff'] = processed_features['initialWindForecast'] \
+                                     - processed_features['latestWindForecast']
+    processed_features['Solar_Frac'] = processed_features['solar'] / processed_features['quantity']
+    processed_features['Wind_Frac'] = (processed_features['wind_off'] + processed_features['wind_on'] )\
+                                      / processed_features['quantity']
+    processed_features['Renewable_Frac'] = processed_features['RenewablePrediction'] / processed_features['quantity']
+    processed_features.indicativeNetImbalanceVolume = processed_features.indicativeNetImbalanceVolume.shift(2)
+    processed_features.indicativeNetImbalanceVolume = processed_features.indicativeNetImbalanceVolume.fillna(0)
+
+    # Rename columns
+    processed_target = processed_target.rename("NIV")
+    processed_features.rename({'quantity': 'Generation', 'systemBuyPrice': 'ImbalancePrice',
+                               'indicativeNetImbalanceVolume': 'Shift_NIV'}, axis='columns', inplace=True)
+
+    return processed_features, processed_target
 
 
-# Sum all the renewables
-renewables_forecast = ['solar', 'wind_off', 'wind_on']
-renewable_generation_forecast.loc[:, 'RenewablePrediction'] = sum_columns(renewable_generation_forecast,
-                                                                          renewables_forecast)
+def log_normalize(series):
+  return series.apply(lambda x: np.log(x+1.0))
 
 
-# Locate the relevant wind data, then calculate the difference between them
-# the wind forecast data is hourly so fill forward to fill NaN values.
-wind_forecast = wind_generation_forecast_and_outturn.loc[:, ['initialWindForecast', 'latestWindForecast',
-                                                             'windOutturn']]
+def clip(series, clip_to_min, clip_to_max):
+  return series.apply(lambda x: (min(max(x, clip_to_min), clip_to_max)))
 
-# New attributes
-wind_forecast['Val_Diff'] = subtract_columns(wind_forecast, 'initialWindForecast', 'latestWindForecast')
-wind_forecast.fillna(method='ffill', inplace=True)
 
-# Define the features needed to train the model
-NIV = derived_system_wide_data.loc[:, 'indicativeNetImbalanceVolume']
-forecast_renewables = renewable_generation_forecast.loc[:, 'RenewablePrediction']
-forecast_demand = forecast_demand.loc[:, 'TSDF']
-forecast_generation = generation_day_ahead.loc[:, 'quantity']
+[processed_features, processed_targets] = preprocess_features(raw_data)
+processed_features_copy = processed_features.copy()
 
-# Combine all features into one data frame
-df = pd.concat([NIV, generation_per_type, apx_price, renewable_generation_forecast, forecast_demand,
-                generation_day_ahead, initial_demand_outturn, interconnectors, loss_of_load_probability,
-                market_index_data, wind_forecast, ], axis=1, sort=True)
+processed_features['APXPrice'] = clip(processed_features['APXPrice'], 0, 200)
+processed_features['Biomass'] = clip(processed_features['Biomass'], 0, 4000)
+processed_features['Nuclear'] = clip(processed_features['Nuclear'], 4000, 10000)
+processed_features['OffWind'] = clip(processed_features['OffWind'], 0, 5000)
+processed_features['OffWind'] = clip(processed_features['OffWind'], 0, 11000)
+processed_features['ImbalancePrice'] = clip(processed_features['ImbalancePrice'], -100, 250)
 
-# Drop the column intenemgeneration as it is NAN
-df = df.drop("intnemGeneration", axis=1)
-df.dropna(inplace=True)
+processed_features['FossilHardCoal'] = log_normalize(processed_features['FossilHardCoal'])
+processed_features['HydroPumpedStorage'] = log_normalize(processed_features['HydroPumpedStorage'])
+processed_features['HydroRunOfRiver'] = log_normalize(processed_features['HydroRunOfRiver'])
+processed_features['Solar'] = log_normalize(processed_features['Solar'])
+processed_features['Other'] = log_normalize(processed_features['Other'])
 
-# Get names of indexes for which column generation has value less than 10GW and drop
-# TODO: Question for A & J: as the data is chronological, can you drop rows or does that create new relationships.
-indexNames = df[df['quantity'] < 10000].index
-df.drop(indexNames, inplace=True)
-df = df.rename({'indicativeNetImbalanceVolume': 'NIV', 'quantity': 'Generation'}, axis='columns')
+# ----------------------------------------------------------------------------------------------------------------------
+# calculate the correlation matrix, isolate the NIV correlations and then order by the abs value (descending)
 
-# After investigating the data these were the categories with the largest correlation to NIV
-#           --------------
-#           cols = ['RenewablePrediction', 'TSDF', 'Generation', 'initialWindForecast', 'latestWindForecast']
-#           --------------
-cols_all = ['Biomass', 'HydroPumpedStorage', 'Other', 'Solar', 'solar', 'wind_off', 'APXPrice', 'initialWindForecast']
+processed_features = processed_features.loc[processed_features.index > 2016000000, :]
+processed_targets = processed_targets.loc[processed_targets.index > 2016000000]
 
-# Take data from 2018 onwards to reduce training time, then split data chronologically.
-# TODO: Question for A & J: when you split the data, you should do this before you investigate relationships, and
-#                     before you split the data should be kept in chronological order.
-df = df.loc[df.index > 2016000000, :]
-train = df.loc[df.index < 2018030000, :]
-validate = df.loc[df.index > 2018030000, :]
+X_train_all = processed_features.loc[processed_features.index < 2018030000, :]
+y_train = processed_targets.loc[processed_targets.index < 2018030000]
 
-# Create X data and then standardise it by subtracting the mean and dividing by the standard deviation.
-X_train = train.loc[:, cols_all]
+X_validate_all = processed_features.loc[processed_features.index > 2018030000, :]
+y_validate = processed_targets.loc[processed_targets.index > 2018030000]
+
+# Normalize the validation data and separate into X and y variables data frames.
+cols_all = ['Shift_NIV', 'ImbalancePrice', 'solar', 'Solar_Frac', 'APXPrice',
+       'Biomass', 'Other', 'wind_off', 'Solar', 'initialWindForecast']
+
+X_train = X_train_all.loc[:, cols_all]
+
 X_train_mean = X_train.mean()
 X_train_std = X_train.std()
-X_norm = 100 * (X_train - X_train_mean) / X_train_std
+X_train = (X_train - X_train_mean) / X_train_std
 
-
-y_train = train.loc[:, 'NIV']
-
-# Normalize the validation data and separate into x and y variables data frames.
-X_validate = validate.loc[:, cols_all]
-X_norm_validate = 100*(X_validate-X_validate.mean())/X_validate.std()
-
-y_validate = validate.loc[:, 'NIV']
+X_validate = X_validate_all.loc[:, cols_all]
+X_validate = (X_validate-X_train_mean)/X_train_std
 
 # --------------------------------------------------------------------------------------
 # Model Training
 # Train each sklearn model
 lin = LinearRegression()
-lin.fit(X_norm, y_train)
+lin.fit(X_train, y_train)
 
 ela = ElasticNet(alpha=171.727)
-ela.fit(X_norm, y_train)
+ela.fit(X_train, y_train)
 
 lass = Lasso(alpha=92.069)
-lass.fit(X_norm, y_train)
+lass.fit(X_train, y_train)
 
 forest_reg = RandomForestRegressor(n_estimators=400, min_samples_split=2, min_samples_leaf=4, max_features='sqrt',
                                    max_depth=10, bootstrap=True,  random_state=42)
-forest_reg.fit(X_norm, y_train)
+forest_reg.fit(X_train, y_train)
 
 # Calculate the predictions from each model.
-y_lin_prediction = lin.predict(X_norm_validate)
+y_lin_prediction = lin.predict(X_validate)
 lin_mse = mean_squared_error(y_validate, y_lin_prediction)
 lin_rme = np.sqrt(lin_mse)
 
 
-y_ela_prediction = ela.predict(X_norm_validate)
+y_ela_prediction = ela.predict(X_validate)
 ela_mse = mean_squared_error(y_validate, y_ela_prediction)
 ela_rme = np.sqrt(ela_mse)
 
-y_lass_prediction = lass.predict(X_norm_validate)
+y_lass_prediction = lass.predict(X_validate)
 lass_mse = mean_squared_error(y_validate, y_lass_prediction)
 lass_rme = np.sqrt(lass_mse)
 
-y_random_forest_prediction = forest_reg.predict(X_norm_validate)
+y_random_forest_prediction = forest_reg.predict(X_validate)
 random_forest_mse = mean_squared_error(y_validate, y_random_forest_prediction)
 random_forest_rme = np.sqrt(random_forest_mse)
 
@@ -180,7 +217,6 @@ static_rmse = np.sqrt(static_mse)
 
 # Cross validation
 def display_scores(scores):
-    print()
     print("Scores:", scores)
     print("Mean:", scores.mean())
     print("Standard deviation:", scores.std())
@@ -188,18 +224,22 @@ def display_scores(scores):
 
 lin_scores = cross_val_score(lin, X_train, y_train, scoring="neg_mean_squared_error", cv=10)
 lin_rmse_scores = np.sqrt(-lin_scores)
+print("Linear Regression Model")
 display_scores(lin_rmse_scores)
 
 lass_scores = cross_val_score(lass, X_train, y_train, scoring="neg_mean_squared_error", cv=10)
 lass_rmse_scores = np.sqrt(-lass_scores)
+print("LASSO Model")
 display_scores(lass_rmse_scores)
 
 ela_scores = cross_val_score(ela, X_train, y_train, scoring="neg_mean_squared_error", cv=10)
 ela_rmse_scores = np.sqrt(-ela_scores)
+print("Elastic Net Model")
 display_scores(ela_rmse_scores)
 
 random_forest_scores = cross_val_score(forest_reg, X_train, y_train, scoring="neg_mean_squared_error", cv=10)
 random_forest_rmse_scores = np.sqrt(-random_forest_scores)
+print("Random Forests Model")
 display_scores(random_forest_rmse_scores)
 
 print()
